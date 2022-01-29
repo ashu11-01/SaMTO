391b4a1ca0f7899950353c2b5ea0011d033efb05
ashu11_01 <writetoashutosh@gmail.com>
2022-01-29 18:31:39 +0530

    Added MongoDB URL in .env

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const router = require('./router')
 const {passport} = require('./middlewares/passport')
 const app = new express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-mongoose.connect('mongodb://localhost:27017/samtoDB', 
+mongoose.connect(process.env.MONGODB_URL, 
 {useNewUrlParser : true, useUnifiedTopology : true}, (err) => {
     const msg = err ? err : 'Connected to database!';
     console.log(msg);
